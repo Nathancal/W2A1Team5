b4956a5d1dafa138891e4e3654d884633a95b11f
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-06 15:29:07 +0000

    Create DiscountCode.cs

---
+++ VapeShop/App_Code/BLL/DiscountCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VapeShop.App_Code.BLL
+{
+    public class DiscountCode
+    {
+        private DateTime dateActive, dateEnd;
+        private int numUsed, discountPerc;
+
+
+        public DiscountCode() { 
+        
+        }
+
+
+    }
+}
