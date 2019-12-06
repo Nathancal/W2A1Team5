f3e26adb0a0d9de9191d88eef306d843d6d121fe
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-06 15:29:04 +0000

    Create ProductRating.cs

---
+++ VapeShop/App_Code/BLL/ProductRating.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VapeShop.App_Code.DAL;
+
+namespace VapeShop.App_Code.BLL
+{
+    public class ProductRating
+    {
+        private int ratingNum, productId, rating, userId;
+        private string userIp, ratingDesc;
+        private DateTime dateSubmitted;
+
+        public ProductRating(int productId, int rating, int userId, string userIp, string ratingDesc, DateTime dateSub)
+        {
+            this.productId = productId;
+            this.ratingNum = rating;
+            this.userId = userId;
+            this.userIp = userIp;
+            this.ratingDesc = ratingDesc;
+            this.dateSubmitted = dateSub;
+           
+        }
+
+        public void createRating() {
+            int retRatingNum = DataAccess.createNewRating(productId, rating, userId, userIp, ratingDesc, dateSubmitted);
+            ratingNum = retRatingNum;          
+        }
+
+
+    }
+}
