b73723dab96435c400692579bf2dff97fbe5b1a7
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-07 13:30:19 +0000

    Update Message.cs

--- VapeShop/App_Code/BLL/Message.cs
+++ VapeShop/App_Code/BLL/Message.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using VapeShop.App_Code.DAL;
+using System.Data;
 
 namespace VapeShop.App_Code.BLL
 {
     public class Message
     {
-
         private int msgId, creatorId, parentMessageId;
         private string subject, messageBody, recepUsername;
         private DateTime createDate;
@@ -21,18 +21,27 @@ namespace VapeShop.App_Code.BLL
             this.createDate = createDate;
         }
 
-        public Message sendMessage() {
-            Message msg = DataAccess.sendMessage(creatorId, parentMessageId, subject, messageBody, createDate, recepientId);
+        public Message() { }
 
-            return msg;
+        public void sendMessage(){
+            DataAccess.sendMessage(creatorId, subject, messageBody, createDate, parentMessageId, recepUsername);
+        }
+
+        public static int getUnreadMessageCount(int userId){
+            int msgCount = DataAccess.getUnreadMessageCount(userId);
+            return msgCount;
+        }
+
+        public void setMessageViewed(int msgId){
+            DataAccess.
 
         }
 
-        public int getMessageId() {
+        public int getMessageId(){
             return msgId;
         }
 
-        public int getCreatorId() {
+        public int getCreatorId(){
             return creatorId;
         }
 
