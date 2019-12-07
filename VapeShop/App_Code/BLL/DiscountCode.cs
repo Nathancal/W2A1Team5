faf69951fe6712e7301b9ba2c676f7a46a444ce1
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-07 12:18:07 +0000

    Update DiscountCode.cs

--- VapeShop/App_Code/BLL/DiscountCode.cs
+++ VapeShop/App_Code/BLL/DiscountCode.cs
@@ -2,18 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VapeShop.App_Code.DAL;
 
 namespace VapeShop.App_Code.BLL
 {
     public class DiscountCode
     {
         private DateTime dateActive, dateEnd;
-        private int numUsed, discountPerc;
+        private int discountPerc;
+        private string code;
 
 
-        public DiscountCode() { 
-        
+        public DiscountCode(string code, DateTime dateActive, DateTime dateEnd, int discountPerc) {
+            this.code = code;
+            this.dateActive = dateActive;
+            this.dateEnd = dateEnd;
+            this.discountPerc = discountPerc;
         }
+
+        public DateTime getDateActive() {
+            return dateActive;
+        }
+
+        public void setDateActive(DateTime dateActive) {
+            this.dateActive = dateActive;
+        }
+
+        public DateTime getDateEnd() {
+            return dateEnd;
+        }
+
+        public void setDateEnd(DateTime dateEnd) {
+            this.dateEnd = dateEnd;
+        }
+
+        public void createNewDiscountCode() {
+            DataAccess.createNewDiscountCode(code, dateActive, dateEnd, discountPerc);
+        }
+
 
 
     }
