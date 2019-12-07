d70222ff5374666a223740484510b4e08da323ee
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-07 15:39:15 +0000

    Update DiscountCode.cs

--- VapeShop/App_Code/BLL/DiscountCode.cs
+++ VapeShop/App_Code/BLL/DiscountCode.cs
@@ -9,15 +9,34 @@ namespace VapeShop.App_Code.BLL
     public class DiscountCode
     {
         private DateTime dateActive, dateEnd;
-        private int discountPerc;
+        private int discountPerc, isActive;
         private string code;
 
 
-        public DiscountCode(string code, DateTime dateActive, DateTime dateEnd, int discountPerc) {
+        public DiscountCode(string code, DateTime dateActive, DateTime dateEnd, int discountPerc, int isActive) {
             this.code = code;
             this.dateActive = dateActive;
             this.dateEnd = dateEnd;
             this.discountPerc = discountPerc;
+            this.isActive = isActive;
+        }
+
+        public DiscountCode(string code, DateTime dateActive, DateTime dateEnd, int discountPerc)
+        {
+            this.code = code;
+            this.dateActive = dateActive;
+            this.dateEnd = dateEnd;
+            this.discountPerc = discountPerc;
+        }
+
+        public DiscountCode() { }
+
+        public string getCode() {
+            return code;
+        }
+
+        public int getDiscountPerc() {
+            return discountPerc;
         }
 
         public DateTime getDateActive() {
@@ -36,8 +55,17 @@ namespace VapeShop.App_Code.BLL
             this.dateEnd = dateEnd;
         }
 
-        public void createNewDiscountCode() {
+        public void createNewDiscountCode(){
             DataAccess.createNewDiscountCode(code, dateActive, dateEnd, discountPerc);
+        }
+
+        public DiscountCode redeemDiscountCode(string pCode){
+
+            DiscountCode dc1 = new DiscountCode();
+            DataAccess.redeemDiscountCode(pCode);
+
+            this.code =
+
         }
 
 
