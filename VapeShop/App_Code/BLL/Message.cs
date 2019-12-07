03ecc102fcd23b6f0457ef36bf096445f35884a3
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-07 12:18:16 +0000

    Create Message.cs

---
+++ VapeShop/App_Code/BLL/Message.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VapeShop.App_Code.DAL;
+
+namespace VapeShop.App_Code.BLL
+{
+    public class Message
+    {
+
+        private int msgId, creatorId, parentMessageId;
+        private string subject, messageBody, recepUsername;
+        private DateTime createDate;
+
+        public Message(int creatorId,  string subject, string messageBody, DateTime createDate, int parentId, string recepUsername) {
+            this.creatorId = creatorId;
+            this.parentMessageId = parentId;
+            this.subject = subject;
+            this.messageBody = messageBody;
+            this.createDate = createDate;
+        }
+
+        public Message sendMessage() {
+            Message msg = DataAccess.sendMessage(creatorId, parentMessageId, subject, messageBody, createDate, recepientId);
+
+            return msg;
+
+        }
+
+        public int getMessageId() {
+            return msgId;
+        }
+
+        public int getCreatorId() {
+            return creatorId;
+        }
+
+        public int getParentMsgId() {
+            return parentMessageId;
+        }
+
+        public void setParentMsgId(int msgId) {
+            this.parentMessageId = msgId;
+        }
+
+        public string getSubject() {
+            return subject;
+        }
+
+        public void setSubject(string subject) {
+            this.subject = subject;
+        }
+
+        public string getMessageBody() {
+            return messageBody;
+        }
+
+        public void setMessageBody(string messageBody) {
+            this.messageBody = messageBody;
+        }
+
+        public DateTime getCreateDate() {
+            return createDate;
+        }
+
+        public void setCreateDate(DateTime createDate) {
+            this.createDate = createDate;
+        }
+    }
+}
