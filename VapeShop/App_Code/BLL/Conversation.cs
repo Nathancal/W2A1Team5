a9c71559195e5a1216695362efabfe0272d5dbc7
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-07 13:30:17 +0000

    Create Conversation.cs

---
+++ VapeShop/App_Code/BLL/Conversation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using VapeShop.App_Code.DAL;
+
+namespace VapeShop.App_Code.BLL
+{
+    public class Conversation
+    {
+
+        public Conversation() { 
+        
+        }
+
+        public static DataSet getConversation() {
+            return DataAccess.getMessages();
+        
+        }
+    }
+}
