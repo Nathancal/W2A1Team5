ccfc6331234779d96b792239897d51c8097c8fe8
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-08 13:14:30 +0000

    Create Invoice.cs

---
+++ VapeShop/App_Code/BLL/Invoice.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VapeShop.App_Code.DAL;
+
+namespace VapeShop.App_Code.BLL
+{
+    public class Invoice
+    {
+
+        private int invoiceNum;
+        private string email, shipMethod;
+        private DateTime orderDate;
+        private double subTotal, shipping, totalCost;
+
+        public Invoice(){ 
+        }
+
+        public Invoice(string invEmail, string invShipMethod, double invSubTotal, double invShipping){
+
+            email = invEmail;
+            shipMethod = invShipMethod;
+            subTotal = invSubTotal;
+            shipping = invShipping;
+            orderDate = DateTime.Now;
+            totalCost = subTotal + shipping;
+
+        }
+
+        public Invoice(int invoiceNum, string invEmail, string invShipMethod, double invSubTotal, double invShipping, DateTime invOrderDate, double invTotalCost)
+        {
+
+            email = invEmail;
+            shipMethod = invShipMethod;
+            subTotal = invSubTotal;
+            shipping = invShipping;
+            orderDate = invOrderDate;
+            totalCost = invTotalCost;
+
+        }
+
+        public void createInvoice(){
+            int invNum = DataAccess.createNewInvoice(email, shipMethod, orderDate, subTotal, shipping, totalCost);
+            invoiceNum = invNum;
+        }
+
+        public Invoice returnNewInvoice(){
+            DataAccess.returnNewInvoice(invoiceNum);
+        }
+
+
+    }
+}
