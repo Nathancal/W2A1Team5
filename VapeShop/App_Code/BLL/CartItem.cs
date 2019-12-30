b516b893d8ca6f9bdd98b2a058d3bb0f3b4083f1
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-30 14:40:18 +0000

    CartItems Updated(VapeShop)

--- VapeShop/App_Code/BLL/CartItem.cs
+++ VapeShop/App_Code/BLL/CartItem.cs
@@ -2,10 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VapeShop.App_Code.DAL;
 
 namespace VapeShop.App_Code.BLL
 {
     public class CartItem
     {
+        private string productName, productType;
+        private double price;
+        private int productId, quantity;
+
+        public CartItem(int prodId, string prodName, string prodType, double price, int prodQuantity) {
+            this.productId = prodId;
+            this.productName = prodName;
+            this.productType = prodType;
+            this.price = price;
+            this.quantity = prodQuantity;
+        }
+
+        public int getProdId() {
+            return productId;
+        }
+
+        public void setProdId(int prodId) {
+            this.productId = prodId;
+        }
+
+        public string getProdName() {
+            return productName;
+        }
+
+        public void setProdName(string name) {
+            this.productName = name;
+        }
+
+        public string getProdType(){
+            return productType;
+        }
+
+        public void setProdType(string type){
+            this.productType = type;
+        }
+
+        public double getProdPrice() {
+            return price;
+        }
+
+        public void setProdPrice(double price) {
+            this.price = price;
+        }
+
+        public int getProdQuantity(){
+            return quantity;
+        }
+
+        public void setProdQuantity(int quantity){
+            this.quantity = quantity;
+        }
+
+        public void createOrderItem(int invoiceNum) {
+            daCartItem.createOrderItem(this, invoiceNum);
+        }
+    
     }
 }
