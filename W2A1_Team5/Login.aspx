390226965fe164dc499040272611d4381c06f17c
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2019-12-30 19:03:52 +0000

    Updated with secure and admin

--- W2A1_Team5/Login.aspx
+++ W2A1_Team5/Login.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/VapeMaster.Master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="VapeMaster.Master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
--- W2A1_Team5/VapeMaster.Master.designer.cs
+++ W2A1_Team5/VapeMaster.Master.designer.cs
@@ -42,6 +42,15 @@ namespace W2A1Team5
         protected global::System.Web.UI.WebControls.Image logo;
 
         /// <summary>
+        /// loginicon control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton loginicon;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
